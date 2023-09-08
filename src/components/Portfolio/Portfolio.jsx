d2727bb33c3c0fd8f47d23e306c4710a82f1dd6d
Nahuel Alexander Coronel <coronel98nahuel@gmail.com>
2023-09-07 23:38:48 -0300

    first commit

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,11 +1,38 @@
 import React from 'react'
 import "./Portfolio.css"
-import { works } from '../data/Works'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Link } from 'react-router-dom'
 
 const Portfolio = () => {
+
+  const works = [
+    {
+        "id": 1,
+        "name": "Book Store Cursos",
+        "url": "https://nahuxc.github.io/Book-Store-cursos/",
+        "img":"Book-Store-Courses-Home.png",
+        "tecnologies" : "HTML, CSS, JS, SASS",
+        "categorias": "Desarrollo Web"
+    },
+    {
+        "id": 2,
+        "name": "E-commerce Nike Project",
+        "url": "https://nahuxc.github.io/Book-Store-cursos/",
+        "img":"E-commerce-Nike.png",
+        "tecnologies" : "JS, React, Node, FireBase",
+        "categorias": "Aplicaciones"
+    },
+    {
+        "id": 3,
+        "name": "Pelicoms",
+        "url": "https://nahuxc.github.io/Book-Store-cursos/",
+        "img":"PeliComs.png",
+        "tecnologies" : "JS, React",
+        "categorias": "Aplicaciones"
+    }
+]
+
   return (
     <div className='sect-portfolio'>
         <h2 className='titlePort'>Portfolio | Proyectos</h2>
