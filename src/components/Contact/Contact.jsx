15c727215a698ef536c79fd0872961bccb8578e9
Nahuel Alexander Coronel <coronel98nahuel@gmail.com>
2023-09-11 09:59:37 -0300

    update-PartServicesResponsive

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,26 +1,29 @@
 import React from 'react'
+import "./Contact.css"
 
 const Contact = () => {
   return (
-    <div>
-      <form action="#" method="post">
-        <div>
-          <p>Name</p>
-          <input type="text" name="name" value="name" required />
-        </div>
-        <div>
-          <p>Surname</p>
-          <input type="text" name="surname" value="surname" required />
-        </div>
-        <div>
-          <p>email</p>
-          <input type="email" name="email" value="email" required />
-        </div>
-        <div>
-          <textarea name="" id="" cols="30" rows="10"></textarea>
-        </div>
-        <input type="submit" value="send" />
-      </form>
+    <div className='contact-form'>
+      <div className='contact-form-content'>
+        <form action="#" method="post">
+          <div>
+            <p>Name</p>
+            <input type="text" name="name" value="name" required />
+          </div>
+          <div>
+            <p>Surname</p>
+            <input type="text" name="surname" value="surname" required />
+          </div>
+          <div>
+            <p>email</p>
+            <input type="email" name="email" value="email" required />
+          </div>
+          <div>
+            <textarea name="" id="" cols="30" rows="10"></textarea>
+          </div>
+          <input type="submit" value="send" />
+        </form>
+      </div>
     </div>
   )
 }
